4f2868c690de3799f333378666d6e401d65cfe22
alimransahin <mdalimransahin@gmail.com>
2024-09-09 08:59:28 +0600

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p className="bg-slate-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel
+        reiciendis perferendis itaque quo, hic sed sit fugiat earum voluptas
+        quis nemo excepturi, vero sequi tenetur, soluta quidem quos deserunt.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
